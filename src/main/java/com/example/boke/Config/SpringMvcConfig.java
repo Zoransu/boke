947a1a0849a3c468e84e88e68cee63e025509c05
Zoran <zoran@zoran.com>
2024-06-20 20:54:21 +0800

    跨域问题3

--- src/main/java/com/example/boke/Config/SpringMvcConfig.java
+++ src/main/java/com/example/boke/Config/SpringMvcConfig.java
@@ -18,7 +18,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,15 +26,6 @@ import java.util.List;
 @EnableSwagger2
 @EnableKnife4j
 public class SpringMvcConfig extends WebMvcConfigurationSupport {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true)
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*");
-    }
 
     @Bean
     public Docket createRestApi() {

5f196c3c0e0872339449d0ab705fbc072b9c702d
Zoran <zoran@zoran.com>
2024-06-24 12:17:51 +0800

    添加评论接口修改

--- src/main/java/com/example/boke/Mapper/CommentMapper.java
+++ src/main/java/com/example/boke/Mapper/CommentMapper.java
@@ -6,8 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CommentMapper {
-    @Insert("INSERT INTO comments (article_id, user_id, comment_date, comment_content, parent_comment_id)" +
-            "VALUES (#{articleId}, #{userId}, #{commentDate}, #{commentContent}, #{parentCommentId})")
+
     void addComment(Comment comment);
 
     void deleteComment(Long commentId, Long userId);
